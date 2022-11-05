0b7cf45fe3e9bbd987ca51b6416b6f0b93085bd9
Tran Hieu <hieutnhe153199@fpt.edu.vn>
2022-11-05 14:37:26 +0700

    finish add submission layout

--- app/src/main/java/com/example/cmsmobile/activity/AddExamSubmissionActivity.java
+++ app/src/main/java/com/example/cmsmobile/activity/AddExamSubmissionActivity.java
@@ -2,7 +2,10 @@ package com.example.cmsmobile.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.cmsmobile.R;
 
@@ -12,5 +15,38 @@ public class AddExamSubmissionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_exam_submission);
+
+        SaveSubmission();
+        CancelSubmission();
+    }
+
+    private void CancelSubmission() {
+        Button cancel_btn = findViewById(R.id.btn_cancel_submission);
+        cancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(AddExamSubmissionActivity.this, ViewExamSubmissionActivity.class);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void SaveSubmission() {
+        Button add_btn = findViewById(R.id.btn_add_submission);
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(AddExamSubmissionActivity.this, ViewExamSubmissionActivity.class);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
