277fe24e38e8a8dc1806fdccc0b7815065866718
NamNgN <doublelift2012@gmail.com>
2022-11-06 23:21:54 +0700

    Search

---
+++ app/src/main/java/com/example/cmsmobile/activity/CourseItemSearchListActivity.java
@@ -0,0 +1,14 @@
+package com.example.cmsmobile.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CourseItemSearchListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_item_search_list);
+    }
+}
