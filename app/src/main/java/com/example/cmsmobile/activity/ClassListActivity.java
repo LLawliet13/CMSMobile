ad699cf1b601327f723d4452cff0348c2956c645
tran dung <dungtnt1303@gmail.com>
2022-11-06 19:01:47 +0700

    add update profile, register

--- app/src/main/java/com/example/cmsmobile/activity/ClassListActivity.java
+++ app/src/main/java/com/example/cmsmobile/activity/ClassListActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.cmsmobile.R;
+import com.example.cmsmobile.repository.ClassRepository;
 
 public class ClassListActivity extends AppCompatActivity {
+
+    int teacherId ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_list);
+//        ClassRepository
     }
 }
