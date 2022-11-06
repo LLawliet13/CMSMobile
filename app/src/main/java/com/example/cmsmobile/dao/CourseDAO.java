7c1355dbac5492db964e3adec67a758e5bb9a6a3
Tran Hieu <hieutnhe153199@fpt.edu.vn>
2022-11-06 18:42:11 +0700

    add course cuccess

--- app/src/main/java/com/example/cmsmobile/dao/CourseDAO.java
+++ app/src/main/java/com/example/cmsmobile/dao/CourseDAO.java
@@ -25,10 +25,12 @@ public interface CourseDAO {
     @Delete
     void deleteCourse(Course course);
 
-    @Query("Select * from Course ")
+    @Query("Select * from Course")
     List<Course> getAllCourse();
 
     @Query("Select * from Course where name = :name ")
     Optional<Course> findByName(String name);
 
+    @Query("Select * from Course where course_id = :id ")
+    Optional<Course> findById(int id);
 }
--- app/src/main/java/com/example/cmsmobile/repository/CourseRepository.java
+++ app/src/main/java/com/example/cmsmobile/repository/CourseRepository.java
@@ -1,4 +1,42 @@
 package com.example.cmsmobile.repository;
 
+import android.content.Context;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import com.example.cmsmobile.dao.AccountDAO;
+import com.example.cmsmobile.dao.CourseDAO;
+import com.example.cmsmobile.entity.Account;
+import com.example.cmsmobile.entity.CMSDatabase;
+import com.example.cmsmobile.entity.Course;
+
+import java.util.List;
+
 public class CourseRepository {
+    private CourseDAO courseDAO;
+
+    public CourseRepository(Context context) {
+        courseDAO = CMSDatabase.getInstance(context).courseDAO();
+    }
+
+    public CourseDAO getCourseDAO() {
+        return courseDAO;
+    }
+
+    public void addCourse(Course course){
+        courseDAO.insertCourse(course);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public Course getCourseById(int id) throws Exception {
+        return courseDAO.findById(id).orElseThrow(()->
+                new Exception("No Course Found")
+        );
+    }
+    public List<Course> getAllCourses(){
+        return courseDAO.getAllCourse();
+    }
+    public void updateCourse(Course course){
+        courseDAO.updateCourse(course);
+    }
 }
