196e4fb6f725ee59d2d73508a05526233528e9e4
Viet Phan <phantuanviet0706@gmail.com>
2022-11-06 16:56:45 +0700

    Fix layout

--- app/src/main/java/com/example/cmsmobile/dao/ClassesDAO.java
+++ app/src/main/java/com/example/cmsmobile/dao/ClassesDAO.java
@@ -1,4 +1,27 @@
 package com.example.cmsmobile.dao;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.cmsmobile.entity.Classes;
+
+import java.util.List;
+
+@Dao
 public interface ClassesDAO {
+
+    @Insert
+    void insert(Classes classes);
+
+    @Update
+    void updateClass(Classes classes);
+
+    @Delete
+    void delete(Classes classes);
+
+    @Query("SELECT * FROM Classes WHERE id = :id")
+    List<Classes> getAll();
 }
--- app/src/main/java/com/example/cmsmobile/entity/CMSDatabase.java
+++ app/src/main/java/com/example/cmsmobile/entity/CMSDatabase.java
@@ -7,12 +7,14 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.example.cmsmobile.dao.AccountDAO;
+import com.example.cmsmobile.dao.ClassesDAO;
 import com.example.cmsmobile.dao.RoleDAO;
 
 @Database(entities = {Role.class,Account.class, Submission.class},version = 1)
 public abstract class CMSDatabase extends RoomDatabase {
     public abstract RoleDAO roleDAO();
     public abstract AccountDAO accountDAO();
+    public abstract ClassesDAO classesDAO();
     private static volatile CMSDatabase INSTANCE;
 
     public static CMSDatabase getInstance(Context context){
